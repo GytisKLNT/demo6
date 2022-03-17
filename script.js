ee2f8a40306201bfb2addc8947bea553ac6c6bd0
GytisKlnt <gytis.kalanta@gmail.com>
2022-03-17 21:23:30 +0200

    Created shop

---
+++ script.js
@@ -0,0 +1,73 @@
+const displayProducts = (item) => {
+  const container = document.querySelector(".container");
+  const productBlock = document.createElement("div");
+  productBlock.className = "product";
+
+  const image = document.createElement("img");
+  image.className = "img";
+  image.src = `${item.image}`;
+  image.alt = "fruit";
+
+  const info = document.createElement("div");
+  info.className = "info";
+
+  const pavadinimas = document.createElement("h3");
+  pavadinimas.className = "pavadinmas";
+  pavadinimas.innerText = `${item.title}`;
+
+  const rating = document.createElement("div");
+  rating.className = "rating";
+  rating.innerHTML = `${`<i class="fa-solid fa-star"></i>`.repeat(
+    item.rating
+  )}`;
+
+  const kaina = document.createElement("h4");
+  kaina.innerText = `$${item.price.toFixed(2)}`;
+
+  const button = document.createElement("button");
+  button.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> Add to Cart`;
+
+  productBlock.appendChild(image);
+  productBlock.appendChild(info);
+  info.appendChild(pavadinimas);
+  info.appendChild(rating);
+  info.appendChild(kaina);
+  info.appendChild(button);
+  container.appendChild(productBlock);
+};
+
+const getData = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return alert(error.message || "An error has happened");
+  }
+};
+
+const dispayItems = async () => {
+  const products = await getData(
+    "https://demo-17-vnoq3.ondigitalocean.app/products"
+  );
+  const ratings = await getData(
+    "https://demo-17-vnoq3.ondigitalocean.app/ratings"
+  );
+
+  const items = products.map((products) => {
+    const custRatings = ratings.filter(
+      (rating) => rating.productId === products.id
+    );
+    const total = custRatings.reduce((c, v) => c + v.rating, 0);
+    return {
+      ...products,
+      rating: Math.round(total / custRatings.length),
+    };
+  });
+
+  items.forEach((item) => {
+    displayProducts(item);
+  });
+};
+
+dispayItems();
